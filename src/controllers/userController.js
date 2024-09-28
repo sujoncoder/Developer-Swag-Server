42c244af30839492a1f241b710916f5fc0deee1b
sujoncoder <sujonsheikh.in@gmail.com>
2024-09-28 22:37:03 +0600

    make service for code reuseablity

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ import { createJsonWebToken } from "../helpers/jsonWebToken.js";
 import { clientUrl, jwtActivitionKey } from "../secret.js";
 import emailWithNodeMailer from "../helpers/email.js";
 import jwt from "jsonwebtoken"
-import handleUserAction from "../services/userService.js";
+import { findUsers, handleUserAction } from "../services/userService.js";
 
 
 
@@ -18,35 +18,14 @@ export const getUsers = async (req, res, next) => {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 5;
 
-        const searchRegExp = new RegExp(".*" + search + ".*", "i");
-
-        const filter = {
-            isAdmin: { $ne: true },
-            $or: [
-                { name: { $regex: searchRegExp } },
-                { email: { $regex: searchRegExp } },
-                { phone: { $regex: searchRegExp } },
-            ]
-        }
-
-        const options = { password: 0 };
-        const users = await User.find(filter, options).limit(limit).skip((page - 1) * limit);
-
-        const count = await User.find(filter).countDocuments();
-
-        if (!users) throw createError(404, "No user found !")
+        const { users, pagination } = await findUsers(search, limit, page)
 
         return successResponse(res, {
             statusCode: 200,
             message: "User were returned successfully",
             payload: {
-                users,
-                pagination: {
-                    totalPages: Math.ceil(count / limit),
-                    currentPage: page,
-                    previousPage: page - 1 > 0 ? page - 1 : null,
-                    nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
-                }
+                users: users,
+                pagination: pagination
             }
         })
     } catch (error) {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,48 @@
 import createError from "http-errors";
 import User from "../models/userModel.js";
+import { successResponse } from "../helpers/responseController.js";
 
-const handleUserAction = async (action, userId) => {
+
+
+// Handle find users
+export const findUsers = async (search, limit, page) => {
+    try {
+        const searchRegExp = new RegExp(".*" + search + ".*", "i");
+
+        const filter = {
+            isAdmin: { $ne: true },
+            $or: [
+                { name: { $regex: searchRegExp } },
+                { email: { $regex: searchRegExp } },
+                { phone: { $regex: searchRegExp } },
+            ]
+        }
+
+        const options = { password: 0 };
+        const users = await User.find(filter, options).limit(limit).skip((page - 1) * limit);
+
+        const count = await User.find(filter).countDocuments();
+
+        if (!users) throw createError(404, "No user found !")
+
+        return {
+            users,
+            pagination: {
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                previousPage: page - 1 > 0 ? page - 1 : null,
+                nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null
+            }
+        }
+
+    } catch (error) {
+        throw error
+    }
+}
+
+
+// Handle user action.
+export const handleUserAction = async (action, userId) => {
     try {
         let update = {};
         let successMessage;
@@ -31,5 +72,3 @@ const handleUserAction = async (action, userId) => {
         throw (error)
     }
 }
-
-export default handleUserAction;
